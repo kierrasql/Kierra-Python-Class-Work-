400fe1a37d6a7504d531c0091463ce3c6b0303f1
Kierra Young <69705702+kierrasql@users.noreply.github.com>
2021-04-05 23:43:27 -0400

    Add files via upload

---
+++ output .py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[23]:
+
+
+PayeeName = "Sam"
+AccountNumber= "4361"
+AccountType= "checking"
+State= "Oregon"
+Country= "US"
+
+
+# In[25]:
+
+
+print(PayeeName, "lives in", State,Country, "he owns a", AccountType, "account that ends in the number :", AccountNumber )
+
+
+# In[ ]:
+
+
+print("Welcome to Sell My Cars! Please the required information: ")
+brand = input("Brand: ")
+model = input("Model: ")
+year = input("Year: ")
+price = eval(input("Asking Price: "))
+
+
+# In[ ]:
+
+
+
+
