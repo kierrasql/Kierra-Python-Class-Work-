a20c34147b7b5d7ed18e9cd52473e5063d24b953
Kierra Young <69705702+kierrasql@users.noreply.github.com>
2021-04-05 23:43:40 -0400

    Add files via upload

---
+++ Input.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print("Please enter the required information: ")
+PayeeName = input("Payee Name: ")
+State = input("State: ")
+Country = input("Country: ")
+Accountnumber = eval(input("Account Number?: "))
+
+
+# In[ ]:
+
+
+
+
